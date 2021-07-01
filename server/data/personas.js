8257c9a48ea0981f135365109dd4fce98bd7ea17
melina-W <meluwaigant@gmail.com>
2021-07-02 10:20:29 +1200

    home route

---
+++ server/data/personas.js
@@ -0,0 +1,36 @@
+const personas = {
+  personas: [
+    {
+      name: 'Keisuke',
+      picture: 'tobeinclude',
+      music: 'Dance',
+      nationality: 'Japanese',
+      food: 'snapper'
+    },
+    {
+      name: 'Kritika',
+      picture: 'tobeinclude',
+      music: 'indi',
+      nationality: 'India',
+      food: 'tank'
+    },
+    {
+      name: 'Westley',
+      picture: '',
+      music: 'rock',
+      nationality: 'Maori',
+      food: 'hāngī'
+    },
+    {
+      name: 'Kris',
+      picture: 'tobeinclude',
+      music: 'rock',
+      nationality: 'British',
+      food: 'pita-pit'
+    }
+  ]
+}
+
+module.exports = {
+  personas
+}
