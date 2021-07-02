3821c38332e8dcbf454b85c2c814376f9b654b18
krjw-00 <kriswd0@gmail.com>
2021-07-02 12:17:33 +1200

    Add some dialogue

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ChatBot from 'react-simple-chatbot'
-import steps from './chat_dialogue'
-// import { ThemeProvider } from 'styled-components'
+import { steps, theme } from './chat_dialogue'
+import { ThemeProvider } from 'styled-components'
 
 function App () {
   return (
     <div>
       <h1>Hello World</h1>
-      <ChatBot steps={steps} />
+      <ThemeProvider theme={theme}>
+        <ChatBot steps={steps} />;
+      </ThemeProvider>
     </div>
   )
 }
--- client/components/chat_dialogue.js
+++ client/components/chat_dialogue.js
@@ -1,24 +1,29 @@
 const steps = [
   {
     id: '1',
-    message: "Morning Team Cactus, it's game day! Are you read to code?",
+    message: "Hey, how u doin'?",
     trigger: '2'
   },
   {
     id: '2',
     options: [
-      { value: 1, label: 'Hell yeah', trigger: '4' },
-      { value: 2, label: 'Yeah na', trigger: '3' }
+      { value: 1, label: 'Great, thanks', trigger: '4' },
+      { value: 2, label: '...Meh', trigger: '3' }
     ]
   },
   {
     id: '3',
-    message: 'Ah no way, well no pressure. Take your time.',
+    message: 'Oh no, what wrong?',
     trigger: '1'
   },
   {
     id: '4',
-    message: "Sweet as, let's get on the keys",
+    message: 'Sweet as',
+    trigger: '1'
+  },
+  {
+    id: '5',
+    message: 'Sweet as',
     end: true
   }
 ]
@@ -36,16 +41,20 @@ const theme = {
   userFontColor: '#4a4a4a'
 }
 
-module.exports = { theme, steps }
-
 /// /////////////////////////////////////////
 /// //////add to file where component is used ////////////
 
-//   import ChatBot from 'react-simple-chatbot'
-//   import dialogue from './chat_dialogue'
+// import ChatBot from 'react-simple-chatbot'
+// import { steps, theme } from './chat_dialogue'
+// import { ThemeProvider } from 'styled-components'
 
-// <ChatBot
-// steps={dialogue}
-// />
+// <ThemeProvider theme={theme}>
+//  <ChatBot steps={steps} />;
+// </ThemeProvider>
 
-module.exports = steps
+module.exports = { steps, theme }
+
+// B:hey how's it going?
+// U: options 1. yeah okay, yeah na
+// U: how's your day going?
+// B: good, I'm eating pancakes
