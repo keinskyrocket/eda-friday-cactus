aa7a7444c198730f4007ba3405eeb85702d5b77f
Keisuke Tanaka <keinskyrocket@icloud.com>
2021-07-02 15:38:31 +1200

    Fix minor styles

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,16 +9,14 @@ function Home (props) {
       <h1>Cactus Chatbox</h1>
       <h2>Jump on a conversation with one of these avatars</h2>
       <main>
-        <section className="container">
-          <>
-              {personas.map(persona => (
-                <article key={persona.name}>
-                  <img src="/images/avatar-01.svg" />
-                  <Link to={`/personas/${persona.name}`}>{persona.name}</Link>
-                </article>
-              ))}        
-          </>
-        </section>
+        <>
+          {personas.map(persona => (
+            <article className="container" key={persona.name}>
+              <img src={persona.picture} />
+              <Link to={`/personas/${persona.name}`}>{persona.name}</Link>
+            </article>
+          ))}
+        </>
       </main>
       <h2>Note: This is a work of fiction. Any similarity to actual persons, living or dead, or actual events, is purely coincidental.</h2>
     </section>
--- client/components/Individual.jsx
+++ client/components/Individual.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+import ChatBot from 'react-simple-chatbot'
+import { steps, theme } from './chat_dialogue'
+import { ThemeProvider } from 'styled-components'
+
 function Individual (props) {
   const { name } = useParams()
   const { personas } = props
@@ -16,7 +20,7 @@ function Individual (props) {
           <h3>
             <ul>
               <li>All we know about {myItem.name} from Chatbox</li>
-              <li>Picture: {myItem.picture}</li>
+              <li>Picture: <img className ='sm-image' src={myItem.picture} /></li>
               <li>Music: {myItem.music}</li>
               <li>Nationality: {myItem.nationality}</li>
               <li>Food: {myItem.food}</li>
@@ -28,7 +32,10 @@ function Individual (props) {
       <section className='right-half'>
         <div className='outer'>
           <div className='inner'>
-            <div className='container'>
+            <div>
+              <ThemeProvider theme={theme}>
+                <ChatBot steps={steps} />
+              </ThemeProvider>
             </div>
           </div>
         </div>
